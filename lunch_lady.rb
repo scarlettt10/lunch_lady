9f6627b61521735482e9f8c479874a800c6b0a70
Robert Scarlett <scarlettt10@msn.com>
2017-08-18 15:17:29 -0600

    place holder files

---
+++ lunch_lady.rb
@@ -0,0 +1,11 @@
+require "pry"
+
+# food and price Hash
+#use class
+# .map functions to total up prices
+# - the user chooses from a list of main dishes
+# - the user chooses 2 side dish items
+# - computer repeats users order
+# - computer totals lunch items and displays total
+
+
